a723fec945ff5487ab686b97d0234fb3a8b76688
Mauri Candia <maurycandia49@gmail.com>
2026-04-27 17:01:16 -0300

    fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/proyecto-final-ia/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -9,7 +10,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
+    sourcemap: false
   },
 })
